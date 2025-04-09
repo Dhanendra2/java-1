8ff1eed9cf2f6e026d8f70d667d0c749a2cb4579
Dhanendra Parihar <you@example.com>
2025-04-09 23:12:23 +0530

    he

---
+++ practice.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class practice {
+    public static void main(String[] args) {
+      //  int a=10;
+        //int b=5;
+       // System.out.println("Addition: " + (a + b));
+//System.out.println("Subtraction: " + (a - b));
+//System.out.println("Multiplication: " + (a * b));
+//System.out.println("Division: " + (a / b));
+
+int a=29;
+int b=4;
+System.out.println("remainder=" +(a%b));
+
+    }
+}
