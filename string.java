1423f81569a5ec555b6a53c1bf2b569cc8f4790e
Dhanendra Parihar <you@example.com>
2025-04-09 22:48:07 +0530

    msg

---
+++ string.java
@@ -0,0 +1,52 @@
+ 
+  import java.util.*;
+  public class string {
+ 
+    public static void main(String[] args) {
+      //  int a=18;
+        //int b=37;
+    //    a=a+b;
+      //  b=a-b;
+        //a=a-b;
+       // System.out.println(a);
+       // System.err.println(b);
+    //second program....
+      
+       Scanner in = new Scanner(System.in) ;
+      // System.out.println("enter the first number");
+     //int a = in.nextInt();
+    //   System.out.println("enter the any arithmetic operator");
+      // char op= in.next().charAt(0);
+//       System.out.println("enter the second  number");
+  //     int b = in.nextInt();
+
+//switch(op) {
+
+  //case '+' :
+  //System.out.println("sum" +(a+b));
+  //break;
+System.out.println("enter the any integer number");
+int a = in.nextInt();
+
+switch(a){
+
+  case 1:
+  System.out.println("one");
+  break;
+  case 2:
+  System.out.println("second");
+  break;
+  case 3:
+  System.out.println("third");
+  break;
+
+  case 4:
+  System.out.println("forth");
+  break;
+
+
+}
+}
+       
+    
+ }
---
+++ tra.java
@@ -0,0 +1,29 @@
+
+
+    import java.util.*;
+
+public class tra {
+   
+   
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      System.out.println("enter the any integer number");
+      int a = in.nextInt();
+      switch (a) {
+         case 1:
+            System.out.println("one");
+            break;
+         case 2:
+            System.out.println("second");
+            break;
+         case 3:
+            System.out.println("third");
+            break;
+         case 4:
+            System.out.println("forth");
+      }
+
+   }
+}
+
