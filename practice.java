71da9e19b8da67f0a2327ecd70d6c1d75e4d41b5
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-04-13 22:15:55 +0530

    Add files via upload

--- practice.java
+++ practice.java
@@ -1,7 +1,18 @@
 import java.util.*;
 
 public class practice {
+   void sum(int a ,int b) {
+     
+      System.out.println(a+b);
+    }
+
+
+    
+  
     public static void main(String[] args) {
+      practice obj=new practice();
+
+      obj.sum(4,5);
       //  int a=10;
         //int b=5;
        // System.out.println("Addition: " + (a + b));
@@ -9,9 +20,86 @@ public class practice {
 //System.out.println("Multiplication: " + (a * b));
 //System.out.println("Division: " + (a / b));
 
-int a=29;
-int b=4;
-System.out.println("remainder=" +(a%b));
+//int a = 29;
+//int b = 4;
+//System.out.println("Remainder: " + (a % b));
+
+//int a=5;
+//int b=10;
+//int temp=a ;
+//a=b;
+//b=temp;
+//System.out.println("a = " +a + ", b =" +b );
+
+//int num =7 ;
+//if ( num %2==0) {
+  //System.out.println("even");
+//}
+//else{
+  //System.out.println("odd");
+//}
+
+// int a=3;
+// int b=3;
+// int c=3;
+// if (a>=b &&  a>=c){
+//   System.out.println("a is greater");
+// }
+// else if ( b>a && b>c ){
+//   System.out.println("b is greater");
+// }else {
+//   System.out.println("c is greater");
+// }
+//  int year =2024 ;
+//  if ( year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)) {
+//    System.out.println("leap year");
+//    }
+//  else{
+//    System.out.println("not a leap year");
+//  }
+
+//  int i=0;
+//  while(i>5) {
+//   System.out.println(i);
+//   i++;
+//  }
+// Scanner in = new Scanner(System.in);
+// System.out.println("enter any charecter" +' ');
+// int a = in.nextInt();
+// System.out.println(a);
+// int a=45;
+// int b=67;
+// a=a+b;
+// b=a-b;
+// a =a-b;
+// System.out.println(a);
+// System.out.println(b);
+
+// Scanner in = new Scanner(System.in);
+// System.out.println( "enter any integrral number ");
+// int a = in.nextInt();
+// if(a%2==0) {
+//   System.out.println("even");
+// }
+// else{
+//   System.out.println("odd");
+// }
+
+// Scanner in = new Scanner(System.in);
+// System.out.println( "enter any integrral number ");
+//  int a = in.nextInt();
+//  if( a>=0){
+//   System.out.println("positive");
+//  }
+//  else if (a<=0){
+//   System.out.println("negative");
+//  }
+//  else {
+//   System.out.println("zero");
+//  }
+ 
+
+
 
     }
 }
