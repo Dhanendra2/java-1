d13a449a759765e85b7062e75ce81637b55605c7
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-04-23 10:48:12 +0530

    Add files via upload

--- string.java
+++ string.java
@@ -1,123 +1,15 @@
- 
-  import java.util.*;
-  public class string {
- 
+public class string{
     public static void main(String[] args) {
-      //  int a=18;
-        //int b=37;
-    //    a=a+b;
-      //  b=a-b;
-        //a=a-b;
-       // System.out.println(a);
-       // System.err.println(b);
-    //second program....
-      
-      // Scanner in = new Scanner(System.in) ;
-      // System.out.println("enter the first number");
-     //int a = in.nextInt();
-    //   System.out.println("enter the any arithmetic operator");
-      // char op= in.next().charAt(0);
-//       System.out.println("enter the second  number");
-  //     int b = in.nextInt();
-
-//switch(op) {
-
-  //case '+' :
-  //System.out.println("sum" +(a+b));
-  //break;
-//System.out.println("enter the any number");
-//int a = in.nextInt();
-
-// switch(a){
-
-//   case 1:
-//   System.out.println("one");
-//   break;
-//   case 2:
-//   System.out.println("second");
-//   break;
-//   case 3:
-//   System.out.println("third");
-//   break;
-
-//   case 4:
-//   System.out.println("forth");
-//   break;
-
-
-// }
-
-//int a=5;
-//int b=10;
-//int temp=a ;
-//a=b;
-//b=temp;
-//System.out.println("a = " +a + ", b =" +b );
-
-//int num =7 ;
-//if ( num %2==0) {
-  //System.out.println("even");
-//}
-//else{
-  //System.out.println("odd");
-//}
-
-// int a=3;
-// int b=4;
-// int c=9;
-// if (a>b &&  a>c){
-//   System.out.println("a is greater");
-// }
-// else if ( b>c ){
-//   System.out.println("b is greater");
-// }else {
-//   System.out.println("c is greater");
-// }
-//  int year =2024 ;
-//  if ( year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)) {
-//    System.out.println("leap year");
-//    }
-//  else{
-//    System.out.println("not a leap year");
-//  }
-
-
-
-
-
-// Print numbers from 1 to N using a for loop
-
-
-//Scanner in = new Scanner(System.in) ;
-// System.out.println("enter the any number");
-// int n = in.nextInt();
-// for (int i=0; i<=n; i++){
-//   System.out.println(i);
-// }
-
-
-
-//Sum of first N natural numbers using while loop
-int i,n =1;
-int sum =0;
-
-
-Scanner in = new Scanner(System.in) ;
-
-
- System.out.println("enter the any number");
- int n = in.nextInt();
- while (i<=n) {
-  sum =sum +1 ;
-  i++;
- }
- System.out.println(+sum);
-
- 
-    }
-}
-
-
-       
+        String str = "helloooo";
+        String newStr = "";
+        for(int i=0; i<str.length(); i++ ){
+            char c = str.charAt(i);
+            if(newStr.indexOf(c)==-1){
+                  newStr+=c;
+                
+            }
+        }           
+            System.out.println(newStr);
     
- 
+}
+}
