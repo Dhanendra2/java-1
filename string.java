8c536a1540c05b530690ccb5000a37eb2ce8b438
Dhanendra2 <dhanendraparihar2@gmail.com>
2025-04-25 13:54:40 +0530

    Add files via upload

--- string.java
+++ string.java
@@ -1,15 +1,40 @@
 public class string{
     public static void main(String[] args) {
-        String str = "helloooo";
-        String newStr = "";
-        for(int i=0; i<str.length(); i++ ){
-            char c = str.charAt(i);
-            if(newStr.indexOf(c)==-1){
-                  newStr+=c;
+
+
+                                                                            //string ..............
+        // String str = "helloooo";
+        // String newStr = "";
+        // for(int i=0; i<str.length(); i++ ){
+        //     char c = str.charAt(i);
+        //     if(newStr.indexOf(c)==-1){
+        //           newStr+=c;
                 
-            }
-        }           
-            System.out.println(newStr);
+        //     }
+        // }           
+            // System.out.println(newStr);
     
-}
+                                                                 // repeat first letter print
+//     String str= "helloo" ;
+//    // String newStr = " ";
+//     for(int i=0; i<str.length(); i++){
+//         char c= str.charAt(i);
+//         if(str.indexOf(c)==-1){
+//             str+=c;
+
+//         }
+    
+//         else{
+//             System.out.println(c);   
+//         }
+//     }     
+   
+    // System.out.println(str);
+                                       //convert string to array
+// 
+  StringBuilder str= new StringBuilder("hello");
+  str.append("hii");
+
+  System.out.println(str);
+    }
 }
